6c85e5195e6fecb582a660f06dd722fbe7acb9dd
isaelEPJ <isael.ijj@gmail.com>
2021-08-17 09:15:08 -0300

    corrigindo detalhes do usuario

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,9 @@ import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
 
 app.listen(3000, () => {
     console.log('Server is Running!!');
